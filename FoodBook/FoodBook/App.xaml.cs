5e6cae03ef3c5b2e219c8e0b596698b701bec801
JochoVoled <johannes@smidelov.com>
2018-01-04 11:50:57 +0100

    Initial commit

--- FoodBook/FoodBook/App.xaml.cs
+++ FoodBook/FoodBook/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FoodBook.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,8 @@ namespace FoodBook
 		public App ()
 		{
 			InitializeComponent();
-
-			MainPage = new FoodBook.MainPage();
-		}
+            MainPage = new Restaurant();
+        }
 
 		protected override void OnStart ()
 		{
